7493acf0f5fee92ef8d2eef6635fa0f7279be0fd
Jason Maverick Diokno Serencio <202180383@psu.palawan.edu.ph>
2024-01-05 03:12:16 +0800

    added test to add

--- tests.py
+++ tests.py
@@ -16,10 +16,18 @@ class MyAppTests(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTrue("test post" in response.data.decode())
 
-    def test_getactors_by_id(self):
+    def test_get_church_by_id(self):
         response = self.app.get("/church/1")
         self.assertEqual(response.status_code, 200)
         self.assertTrue("test edit" in response.data.decode())
+        
+    def test_add_church(self):
+        data = {
+                    "conference_id": 18,
+                    "details": "This is a test post in unit test.",
+                }
+        response = self.app.post('/church', json=data)
+        self.assertEqual(response.status_code, 201)
 
 
 if __name__ == "__main__":
