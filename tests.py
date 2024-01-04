c5080dd435a9875c4e328e8a6182b013bf5cf328
Jason Maverick Diokno Serencio <202180383@psu.palawan.edu.ph>
2024-01-05 03:36:40 +0800

    added a test for update

--- tests.py
+++ tests.py
@@ -21,24 +21,33 @@ class MyAppTests(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTrue("test edit" in response.data.decode())
         
-    def test_add_church(self):
-        data = [{
-                    "conference_id": 10,
-                    "details": "This is a fourth test post in unit test adding two churches again.",
-                },{
-                    "conference_id": 20,
-                    "details": "This is to be deleted.",
-                }]
-        response = self.app.post('/church', json=data)
-        self.assertEqual(response.status_code, 201)
-        
     # def test_add_church(self):
-    #     data = {
-    #                 "conference_id": 18,
-    #                 "details": "This is a third test post in unit test adding only one church.",
-    #             }
+    #     data = [{
+    #                 "conference_id": 10,
+    #                 "details": "This is a fourth test post in unit test adding two churches again.",
+    #             },{
+    #                 "conference_id": 20,
+    #                 "details": "This is to be deleted.",
+    #             }]
     #     response = self.app.post('/church', json=data)
     #     self.assertEqual(response.status_code, 201)
+        
+    # # def test_add_church(self):
+    # #     data = {
+    # #                 "conference_id": 18,
+    # #                 "details": "This is a third test post in unit test adding only one church.",
+    # #             }
+    # #     response = self.app.post('/church', json=data)
+    # #     self.assertEqual(response.status_code, 201)
+    
+    def test_update_church(self):
+        id = 25 
+        data = {
+            "conference_id": 4,
+            "details": "this is edited",
+        }
+        response = self.app.put(f'/church/{id}', json=data)
+        self.assertEqual(response.status_code, 200)
 
 
 if __name__ == "__main__":
