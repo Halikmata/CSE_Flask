fe6b13eb5732a383c2289b4d7b5384ff14142c3b
Jason Maverick Diokno Serencio <202180383@psu.palawan.edu.ph>
2024-01-05 03:31:46 +0800

    fixed the test for add

--- tests.py
+++ tests.py
@@ -22,12 +22,23 @@ class MyAppTests(unittest.TestCase):
         self.assertTrue("test edit" in response.data.decode())
         
     def test_add_church(self):
-        data = {
-                    "conference_id": 18,
-                    "details": "This is a test post in unit test.",
-                }
+        data = [{
+                    "conference_id": 10,
+                    "details": "This is a fourth test post in unit test adding two churches again.",
+                },{
+                    "conference_id": 20,
+                    "details": "This is to be deleted.",
+                }]
         response = self.app.post('/church', json=data)
         self.assertEqual(response.status_code, 201)
+        
+    # def test_add_church(self):
+    #     data = {
+    #                 "conference_id": 18,
+    #                 "details": "This is a third test post in unit test adding only one church.",
+    #             }
+    #     response = self.app.post('/church', json=data)
+    #     self.assertEqual(response.status_code, 201)
 
 
 if __name__ == "__main__":
